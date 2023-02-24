d113e639faf9fd6c73af424aa5c07267d241fd02
waleediqbal1717 <waleediqbal1717@gmail.com>
2023-02-24 23:47:11 +0500

    feat: Type defined for user

---
+++ src/managers/types/user.tsx
@@ -0,0 +1,10 @@
+export type USER_TYPE = {
+  _id?: string;
+  fname?: string;
+  lname?: string;
+  username?: string;
+  cnic?: string;
+  phone?: string;
+  email?: string;
+  password?: string;
+};
