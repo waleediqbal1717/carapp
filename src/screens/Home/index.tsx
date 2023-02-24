dccef31ac1412f3436450809c0ca1fb8e189b95f
waleediqbal1717 <waleediqbal1717@gmail.com>
2023-02-24 23:46:22 +0500

    feat: Home Screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StatusBar,
+  RefreshControl,
+  TouchableOpacity,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+import { observer } from "mobx-react";
+import { Navigation } from "react-native-navigation";
+import styles from "./styles";
+import { usermanager } from "../../managers/UserManager";
+import theme from "../../theme";
+import { carmanager } from "../../managers/CarManager";
+import { responsiveHeight } from "react-native-responsive-dimensions";
+import { HOME_NAV } from "../../navigation";
+
+const Home = observer(() => {
+  const renderItems = ({ item, index }) => {
+    return (
+      <View style={styles.CarBox}>
+        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.makeTitle}>
+          {item.make}
+        </Text>
+        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.countTitle}>
+          ({carmanager.getMakeCount(item.make)})
+        </Text>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.Container}>
+      <Text style={styles.title}>
+        Total Registered Cars: {carmanager.registeredCarsList.length}
+      </Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={{ marginVertical: responsiveHeight(1) }}
+        columnWrapperStyle={{ justifyContent: "space-around" }}
+        initialNumToRender={8}
+        numColumns={2}
+        maxToRenderPerBatch={8}
+        data={carmanager.getUniqueMake()}
+        extraData={carmanager.registeredCarsList}
+        renderItem={renderItems}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <TouchableOpacity
+        style={styles.addCarButton}
+        onPress={() => Navigation.push(HOME_NAV, add - c)}
+      >
+        <Text style={styles.buttonText}>Add Car</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+});
+export default Home;
---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,65 @@
+import theme from "../../theme";
+import { StyleSheet, Dimensions } from "react-native";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
+const windowHeight = Dimensions.get("window").height;
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+  CarBox: {
+    height: responsiveHeight(20),
+    width: responsiveWidth(46),
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    textAlign: "center",
+    backgroundColor: theme.colors.primary,
+    elevation: 5,
+    borderRadius: 10,
+    marginVertical: responsiveHeight(1),
+    opacity: 0.7,
+  },
+  makeTitle: {
+    color: theme.colors.secondary,
+    fontSize: responsiveFontSize(2),
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  buttonText: {
+    color: theme.colors.background,
+    fontSize: responsiveFontSize(2),
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  countTitle: {
+    color: theme.colors.dark,
+    fontSize: responsiveFontSize(1.7),
+    textAlign: "center",
+    marginTop: 5,
+  },
+  title: {
+    color: theme.colors.secondary,
+    fontSize: responsiveFontSize(2.2),
+    textAlign: "center",
+    fontWeight: "bold",
+    marginVertical: responsiveHeight(2),
+  },
+  addCarButton: {
+    height: responsiveHeight(7),
+    width: responsiveWidth(95),
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.secondary,
+    borderRadius: 10,
+    position: "absolute",
+    bottom: responsiveHeight(2),
+  },
+});
+
+export default styles;
