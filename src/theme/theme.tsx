c5b088a1688b469392120761993c273c01e926c9
waleediqbal1717 <waleediqbal1717@gmail.com>
2023-02-24 23:45:02 +0500

    feat: Theme setup

---
+++ src/theme/theme.tsx
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#6CADD7',
+    secondary: '#054E7B',
+    dark: '#2C2C2C',
+    grey: '#EFEFEF',
+    strokeGrey: '#B1B1B1',
+    background: '#fff',
+  },
+};
+
+export default theme;
